b9a9f6e5d915845af6747137b824e8ad8d2e9eb4
GrypenBrigade <robloxbullcrap@gmail.com>
2026-03-05 22:33:47 +0800

    Using vite and tailwindcss

---
+++ grypenapp/src/pages/Home.tsx
@@ -0,0 +1,13 @@
+
+export function Home() {
+    return (
+        <div className="bg-slate-800 text-white h-screen flex flex-col items-center justify-center">
+            <h1 className="text-4xl p-4">
+                <span className="text-red-500">This </span> 
+                <span className="text-yellow-500">is </span> 
+                <span className="text-green-500">a </span> 
+                <span className="text-blue-500">website</span></h1>
+            <p className="text-purple-500">And you will like it</p>
+        </div>
+    );
+}
